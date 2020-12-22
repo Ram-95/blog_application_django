bb20fdbe26fe5950e501dab5255d4aee14e4201e
Ram Babu <ram.kottapally@gmail.com>
2020-12-22 15:30:16 +0530

    UI Changes in index.html

--- blogs/static/blogs/script.js
+++ blogs/static/blogs/script.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
             $(this).remove();
         });
     }, 5000);
+
+    /* Functionality to Like and Unlike a button */ 
+    $(document).on("click", ".like_post", function () {
+        //alert('Like button clicked');
+        // Getting the POST_ID from the like button clicked
+        post_id = $(this).closest('article').attr('id');
+        alert('Post Liked: ' + post_id);
+    });
 });
