df2d44871caac91d84b6b8abe5ce502b936ffc73
Ram Babu <ram.kottapally@gmail.com>
2020-12-24 21:31:55 +0530

    Added buttons to update, create and delete posts

--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
 from .models import Blog
 from django.contrib.auth.models import User
 from django.db.models import Count
+from django.contrib import messages
 # Class based View
 from django.views.generic import (ListView, CreateView, UpdateView, DeleteView)
 from django.views.decorators.csrf import csrf_exempt
@@ -86,6 +87,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
 # Class based view to Update a post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Blog
@@ -94,7 +96,9 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
     # Checks if the post that is to be updated belongs to the current logged user
+
     def test_func(self):
         post = self.get_object()
         # Checks if the author of post is same as the logged in user
@@ -102,24 +106,25 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
+
 # Class based view to Update a post
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Blog
     success_url = '/'
     # Checks if the post that is to be deleted belongs to the current logged user
+
     def test_func(self):
         post = self.get_object()
         # Checks if the author of post is same as the logged in user
         if self.request.user == post.author:
             return True
         return False
-
 
 
 def view_post(request, pk):
     # Shows a particular post based on it's ID passed via URL
     blog = get_object_or_404(Blog, pk=pk)
-    title = 'Post ' + str(pk)
+    title = blog.title
     context = {
         'blog': blog,
         'title': title,
@@ -133,20 +138,31 @@ def vote_up(request):
     if request.method == 'POST':
         post_id = request.POST['post_id']
         post = Blog.objects.get(id=post_id)
-        post.likes += 1
-        post.save()
-        return HttpResponse('Upvote Done')
+        if request.user != post.author:
+            post.likes += 1
+            post.save()
+            print('Upvote Success')
+            return HttpResponse('Upvote Done')
+        else:
+            print('You cannot like your own post!')
+            return HttpResponse('You cannot like your own post!')
     else:
         return HttpResponse('Request Method is not Post.')
 
 
 @csrf_exempt
+@login_required
 def vote_down(request):
     if request.method == 'POST':
         post_id = request.POST['post_id']
         post = Blog.objects.get(id=post_id)
-        post.likes -= 1
-        post.save()
-        return HttpResponse('Downvote Done')
+        if request.user != post.author:
+            post.likes -= 1
+            post.save()
+            print('Downvote Success')
+            return HttpResponse('Downvote Done')
+        else:
+            print('You cannot downvote your own post!')
+            return HttpResponse('You cannot downvote your own post!')
     else:
         return HttpResponse('Request Method is not Post.')
