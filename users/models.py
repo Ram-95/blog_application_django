73e5e798895d482d3188057ea955c0c6e61b41cb
Ram Babu <ram.kottapally@gmail.com>
2020-12-31 14:02:33 +0530

    Decreasing Profile Image size to 300x300 - Done

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,5 +8,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
+
+    # Overriding the default save() method. To decrease the resolution of Profile Images
+    def save(self):
+        # saves the data of Profile class
+        super().save()
+        img = Image.open(self.profile_pic.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
 
     
