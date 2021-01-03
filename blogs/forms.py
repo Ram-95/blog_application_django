c87b5ee0d58e48f3a4eda79e474ba4b9fff72054
Ram Babu <ram.kottapally@gmail.com>
2021-01-03 13:36:36 +0530

    Changed UI for comments

--- blogs/forms.py
+++ blogs/forms.py
@@ -2,6 +2,14 @@ from django import forms
 from .models import Blog_comments
 
 class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        # Removes the label for 'content' field
+        self.fields['content'].label = False
+    
     class Meta:
         model = Blog_comments
+        # To override the textarea of crispy-forms to user 3 rows in the textarea
+        widgets = {'content': forms.Textarea(attrs={'rows': 3})}
         fields = ['content']
+        
