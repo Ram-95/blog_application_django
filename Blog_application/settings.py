089d917ebfcba56079e5ab65a032c0b740951240
Ram Babu <ram.kottapally@gmail.com>
2021-02-05 14:35:26 +0530

    Minor code changes

--- Blog_application/settings.py
+++ Blog_application/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'nb#)47941bw)%y4ru@di1ux8+=ah9p3v%m5b)&av+ew^c$-uh)'
 DEBUG = True
 
 # To check the site on Mobile and localhost
-#ALLOWED_HOSTS = ['192.168.1.3', 'localhost']
+#ALLOWED_HOSTS = ['192.168.1.8', 'localhost']
 ALLOWED_HOSTS = []
 
 
