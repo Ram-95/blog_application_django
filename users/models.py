1c3b0556a8223721b083c3831aca6ced798e4973
Ram Babu <ram.kottapally@gmail.com>
2021-03-03 17:48:30 +0530

    Changed __str__ of Followers

--- users/models.py
+++ users/models.py
@@ -25,6 +25,6 @@ class Followers(models.Model):
     followers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
 
     def __str__(self):
-        return f'{self.user.username} - {self.followers.username}'
+        return f'{self.user.username} followed by {self.followers.username}'
 
 
