1da02a26dc51b2569394e5eb630d8cc1523ffbfd
Ram Babu <ram.kottapally@gmail.com>
2021-04-04 15:15:34 +0530

    Modified notifications_exist

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -2,7 +2,7 @@ from .models import Notification
 
 def notifications_exist(request):
     if request.user.is_authenticated:
-        f = Notification.objects.filter(receiver=request.user).count()
+        f = Notification.objects.filter(receiver=request.user, is_read=False).count()
         status = True if f > 0 else False
     else:
         status = False
