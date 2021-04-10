ba08ed2711be831dca3a21dc4ccb794e0aaf6bd9
Ram Babu <ram.kottapally@gmail.com>
2021-04-10 20:57:29 +0530

    Modifed to avoid errors on logout

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,12 +1,16 @@
 from .models import Notification
 
+
 def notifications_exist(request):
     if request.user.is_authenticated:
-        f = Notification.objects.filter(receiver=request.user, is_read=False).count()
+        f = Notification.objects.filter(
+            receiver=request.user, is_read=False).count()
         status = True if f > 0 else False
+        return {
+            'status': status,
+            'notif_count': f,
+        }
     else:
         status = False
-    return {
-        'status': status,
-        'notif_count': f,
-    }
+
+    return {'status': status}
