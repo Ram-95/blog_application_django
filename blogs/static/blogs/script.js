b36c19e8193c0a2a7a66f9cdcc82261182d774d3
Ram Babu <ram.kottapally@gmail.com>
2021-06-25 18:52:35 +0530

    Add notification feature events

--- blogs/static/blogs/script.js
+++ blogs/static/blogs/script.js
@@ -220,4 +220,26 @@ $(document).ready(function () {
             }
         });
     });
+
+    $('#noti_Button').click(function () {
+        // TOGGLE (SHOW OR HIDE) NOTIFICATION WINDOW.
+        $('#notifications').fadeToggle('fast', 'linear', function () {
+            if ($('#notifications').is(':hidden')) {
+            }
+        });
+
+        $('#noti_counter').fadeOut('slow');     // HIDE THE COUNTER.
+
+        return false;
+    });
+
+    // HIDE NOTIFICATIONS WHEN CLICKED ANYWHERE ON THE PAGE.
+    $(document).click(function () {
+        $('#notifications').hide();
+    });
+
+    /*
+    $('#notifications').click(function () {
+        return false;       // DO NOTHING WHEN CONTAINER IS CLICKED.
+    });*/
 });
