04ea47355eb39ea5fd9fd7fc8f83bcbc0ad6afa5
Ram Babu <ram.kottapally@gmail.com>
2021-06-25 18:52:16 +0530

    Send unread_notifications to base.html

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -3,11 +3,14 @@ from .models import Notification
 
 def notifications_exist(request):
     if request.user.is_authenticated:
-        f = len(Notification.objects.get_notification_count(request.user))
+        unread_notifs = Notification.objects.get_notification_count(request.user)
+        f = len(unread_notifs)
+        
         status = True if f > 0 else False
         return {
             'status': status,
             'notif_count': f,
+            'unread_notifs': unread_notifs,
         }
     else:
         status = False
