ce183779093f19c3b51026876f4910b99950fbeb
Ram Babu <ram.kottapally@gmail.com>
2021-07-13 13:42:23 +0530

    Remove 'view_profile' & changed 'profile' route

--- users/urls.py
+++ users/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name="password_reset_complete"),
-    path('profile/', views.profile, name='profile'),
+    path('profile/<str:username>/', views.profile, name='profile'),
     path('update-profile/', views.update_profile, name="update_profile"),
     path('follow/', views.follow, name="follow"),
     path('unfollow/', views.unfollow, name="unfollow"),
     path('view_followers/<str:username>/', views.view_followers, name="view_followers"),
-    path('view_profile/<str:username>/', views.view_profile, name='view_profile'),
+
 ]
