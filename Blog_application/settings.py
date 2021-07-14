b0d156a4860bff38e2331cca1c04c00af605dcb2
Ram Babu <ram.kottapally@gmail.com>
2021-07-14 19:28:12 +0530

    Minor changes

--- Blog_application/settings.py
+++ Blog_application/settings.py
@@ -131,9 +131,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
+'''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+'''
 
 
 # Tells cripsy forms to use bootstrap 4
