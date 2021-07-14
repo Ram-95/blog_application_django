c215ffe7a32f952c9c851d2c1adc7527d4bcaa99
Ram Babu <ram.kottapally@gmail.com>
2021-07-14 19:08:49 +0530

    Modified settings - STATICFILES_DIRS

--- Blog_application/settings/development.py
+++ Blog_application/settings/development.py
@@ -127,8 +127,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Tells cripsy forms to use bootstrap 4
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- Blog_application/settings/production.py
+++ Blog_application/settings/production.py
@@ -132,6 +132,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Tells cripsy forms to use bootstrap 4
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
