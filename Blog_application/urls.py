38c4f2a101795da1ebe11dd7a5427492d78d65d4
Ram Babu <ram.kottapally@gmail.com>
2021-07-15 14:39:07 +0530

    Add MEDIA_URL and MEDIA_ROOT to urls

--- Blog_application/urls.py
+++ Blog_application/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('users.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
