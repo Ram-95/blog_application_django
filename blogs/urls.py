26b189a9fa15853865b4b78ed2f400106b3f335d
Ram Babu <ram.kottapally@gmail.com>
2021-07-26 11:05:17 +0530

    Views post even if slug is not passed in URL

--- blogs/urls.py
+++ blogs/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create_post/', PostCreateView.as_view(), name="create_post"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="update_post"),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="delete_post"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="view_post"),
     path('post/<int:pk>/<slug:slug>/', PostDetailView.as_view(), name="view_post"),
     path('delete_comment/', views.delete_comment, name="delete_comment"),
     path('edit_comment/', views.edit_comment, name="edit_comment"),
