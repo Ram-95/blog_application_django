4e1e45c044db099a4475aeedc24966d5d419cf4b
Ram Babu <ram.kottapally@gmail.com>
2021-08-25 18:23:03 +0530

    Commented django_heroku.settings()

--- Blog_application/settings/development.py
+++ Blog_application/settings/development.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
@@ -227,4 +228,5 @@ AWS_S3_ADDRESSING_STYLE = "virtual"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals())
+
+#django_heroku.settings(locals())
