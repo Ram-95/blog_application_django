5fd6321947a4b469adfc4282864d5c86ea499386
Ram Babu <ram.kottapally@gmail.com>
2021-09-02 17:43:01 +0530

    Can only update email

--- users/forms.py
+++ users/forms.py
@@ -34,7 +34,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields =  ['email']
 
 
 # Updates the Profile Model
